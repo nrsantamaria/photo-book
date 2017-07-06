6745cffff07ca1e48dfd00ebe26ec435b0ec9793
James Higgins and Nicole Santamaria <nicole@>
2017-07-06 11:46:49 -0700

    add image spec

--- spec/factories.rb
+++ spec/factories.rb
@@ -5,5 +5,13 @@ FactoryGirl.define do
     password('123456')
     admin false
     avatar File.new(Rails.root + 'spec/support/missing.png')
+    id 1
+  end
+
+  factory :image do
+    name('Picture')
+    description('Pretty picture')
+    user_id 1
+    avatar File.new(Rails.root + 'spec/support/missing.png')
   end
 end
