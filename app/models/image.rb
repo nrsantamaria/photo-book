3e152b2c8da2e691962396684a9c9fe63f278f58
James Higgins and Nicole Santamaria <james@>
2017-07-06 10:48:21 -0700

    add paperclip shoulda matchers for image specs

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,8 +1,7 @@
 class Image < ActiveRecord::Base
   belongs_to :user
   has_many :comments
-  validates :name, :presence => true
-  validates :description, :presence => true
+  validates :name, :description, :avatar, :presence => true
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 end
--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -3,4 +3,9 @@ require 'rails_helper'
 describe Image do
   it { should validate_presence_of :name}
   it { should validate_presence_of :description}
+  it { should have_attached_file(:avatar) }
+  it { should validate_attachment_presence(:avatar) }
+  it { should validate_attachment_content_type(:avatar).
+               allowing('image/png', 'image/gif').
+               rejecting('text/plain', 'text/xml') }
 end
